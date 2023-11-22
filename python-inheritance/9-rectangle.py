bb8cd3372f24a6759ceca63e55422e83028d6923
AngeGitego <k.gitego@alustudent.com>
2023-11-21 23:25:59 -0800

    Add new features

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-'''Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)'''
-
-
+'''Module for Rectangle class.'''
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    '''Rectangle class that inherits the BaseGeometry '''
-  def __init__(self, width, height):
+    '''A subclass representing a rectangle.'''
+    def __init__(self, width, height):
         '''Constructor.'''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
