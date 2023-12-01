73d8fe649a94ea524c9f17a0d7148520223c43a4
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2023-12-01 09:20:58 -0800

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a silent GET request using curl, and displays the size of the response body in bytes.
-curl -sI "$1" | grep -i 'content-length' | awk '{print $2}'
+#Send request and get the size of the response body in bytes
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
